8b17d77ee6d00050b484f9cf8776a4eacac42e66
FEZ-HJ <fez_hj@163.com>
2020-04-20 18:04:33 +0800

    手动获取Bean

--- src/main/java/com/dream/steam/free/freesteam/service/OperationRecordService.java
+++ src/main/java/com/dream/steam/free/freesteam/service/OperationRecordService.java
@@ -2,17 +2,16 @@ package com.dream.steam.free.freesteam.service;
 
 import com.dream.steam.free.freesteam.entity.OperationRecord;
 import com.dream.steam.free.freesteam.repository.OperationRecordRepository;
-import com.dream.steam.free.SpringContextUtil;
+import com.dream.steam.free.SpringUtil;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by H.J
  * 2019/11/8
  */
-@Service
 public class OperationRecordService {
 
     public static void save(OperationRecord operationRecord){
-        SpringContextUtil.getBean(OperationRecordRepository.class).save(operationRecord);
+        SpringUtil.getBean(OperationRecordRepository.class).save(operationRecord);
     }
 }
